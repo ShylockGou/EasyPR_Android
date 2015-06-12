8f50df9712d553ca597bdf809f70bea053145154
ShylockGou <goudezong@163.com>
2015-06-12 10:03:52 +0800

    init

---
+++ app/src/main/jni/src/include/prep.h
@@ -0,0 +1,19 @@
+
+#ifndef __PREP_H__
+#define __PREP_H__
+
+#include "opencv2/opencv.hpp"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <assert.h>
+#include <algorithm>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+using namespace cv;
+
+#endif
+/* endif __PREP_H__ */
---
+++ app/src/main/obj/local/armeabi-v7a/objs/imageproc/core/plate_recognize.o.d
@@ -0,0 +1,236 @@
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/obj/local/armeabi-v7a/objs/imageproc/core/plate_recognize.o: \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/plate_recognize.cpp \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/plate_recognize.h \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/prep.h \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/opencv.hpp \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/core/core_c.h \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/core/types_c.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/assert.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/string.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/strings.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/memory.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/math.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/limits.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /Library/Android/ndk/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/core/core.hpp \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/core/version.hpp \
+ /Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm \
+ /Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath \
+ /Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef \
+ /Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/complex \
+ /Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/map \
+ /Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/vector \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/core/operations.hpp \
+ /Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/limits \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/core/mat.hpp \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/flann/miniflann.hpp \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/flann/defines.h \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/flann/config.h \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/imgproc/imgproc_c.h \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/imgproc/types_c.h \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/imgproc/imgproc.hpp \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/photo/photo.hpp \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/photo/photo_c.h \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/video/video.hpp \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/video/tracking.hpp \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/video/background_segm.hpp \
+ /Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/list \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/features2d/features2d.hpp \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/objdetect/objdetect.hpp \
+ /Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/deque \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/calib3d/calib3d.hpp \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/ml/ml.hpp \
+ /Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/iostream \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/highgui/highgui_c.h \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/highgui/highgui.hpp \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/contrib/contrib.hpp \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/contrib/retina.hpp \
+ /Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/valarray \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/contrib/openfabmap.hpp \
+ /Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/set \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/plate_detect.h \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/plate_locate.h \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/plate_judge.h \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/features.h \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/chars_recognise.h \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/chars_segment.h \
+ /Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/chars_identify.h
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/plate_recognize.h:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/prep.h:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/opencv.hpp:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/core/core_c.h:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/core/types_c.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/string.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/math.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/Library/Android/ndk/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/core/core.hpp:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/core/version.hpp:
+
+/Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/algorithm:
+
+/Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/cmath:
+
+/Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/cstddef:
+
+/Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/complex:
+
+/Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/map:
+
+/Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/vector:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/core/operations.hpp:
+
+/Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/limits:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/core/mat.hpp:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/flann/miniflann.hpp:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/flann/defines.h:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/flann/config.h:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/imgproc/imgproc_c.h:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/imgproc/types_c.h:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/imgproc/imgproc.hpp:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/photo/photo.hpp:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/photo/photo_c.h:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/video/video.hpp:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/video/tracking.hpp:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/video/background_segm.hpp:
+
+/Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/list:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/features2d/features2d.hpp:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/objdetect/objdetect.hpp:
+
+/Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/deque:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/calib3d/calib3d.hpp:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/ml/ml.hpp:
+
+/Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/iostream:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/highgui/highgui_c.h:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/highgui/highgui.hpp:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/contrib/contrib.hpp:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/contrib/retina.hpp:
+
+/Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/valarray:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//../../../../../openCVLibrary249/src/main/jni/include/opencv2/contrib/openfabmap.hpp:
+
+/Library/Android/ndk/sources/cxx-stl/gnu-libstdc++/4.6/include/set:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/plate_detect.h:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/plate_locate.h:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/plate_judge.h:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/features.h:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/chars_recognise.h:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/chars_segment.h:
+
+/Users/Shylock/Workspaces/Android/noopu/EasyPR_Android/app/src/main/jni/src//core/../include/chars_identify.h:
